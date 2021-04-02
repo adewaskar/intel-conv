3520f07542baf3988d76d3c59e358306e8fcc99b
adewaskar <a.dewaskar@yahoo.com>
2021-04-02 09:51:08 +0530

    added name search

--- src/components/UserTranscript.js
+++ src/components/UserTranscript.js
@@ -2,7 +2,7 @@ import './Index.css'
 
 export const UserTranscript = ({words,startTime,index,wordClick,currentTime,searchText})=>
 {
-    
+    const WORDS = JSON.parse(JSON.stringify(words));
     return <div className={`user-transcript ind-${index}`}>
         <div className="row">
             {index===1?<div className="col-md-1"></div>:null}
@@ -10,13 +10,14 @@ export const UserTranscript = ({words,startTime,index,wordClick,currentTime,sear
                 <div className="time"><b>{startTime}</b></div>
             </div>
             <div className={`col-md-${index === 1 ? '10' : '9'} text`}>
-                {words.map(w => {
-                        // const isSearch = searchText?(w.word.toLowerCase().includes(searchText.toLowerCase())):false;
-                        // if (isSearch)
-                        // {
-                        //     w.word.isSearchText = true;                
-                        // }
-                    
+                {WORDS.map(w => {
+                    console.log(w,searchText,'searchTextsearchTextsearchTextsearchText')
+                        const isSearch = searchText?(w.word.toLowerCase().includes(searchText.toLowerCase())):false;
+                        if (isSearch)
+                        {
+                            w.isSearchText = true;                
+                        }
+                        
                     return <span className={`${((currentTime >= w.startTime) && (currentTime < w.endTime)) ? 'active' : ''} ${w.isSearchText ? 'search-text' : ''} word`} onClick={() => wordClick(w)}>{w.word} </span>
                 })}
             </div>
